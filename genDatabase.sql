f7749be32ca24cd530a13ebe2c9d52d901022a32
ILeoI <leoj.whiteside@gmail.com>
2023-12-06 16:54:12 +0930

    changed thinfs 2

--- genDatabase.sql
+++ genDatabase.sql
@@ -4,31 +4,31 @@ CREATE DATABASE sharing;
 
 USE sharing;
 
-CREATE TABLE `Groceries` (
-  `groceryID` int(11) NOT NULL PRIMARY KEY,
-  `name` varchar(14) DEFAULT NULL
+CREATE TABLE Groceries (
+  groceryID int(11) NOT NULL PRIMARY KEY,
+  name varchar(14) DEFAULT NULL
 );
 
-INSERT INTO `Groceries` (`groceryID`, `name`) VALUES
+INSERT INTO Groceries (groceryID, name) VALUES
 (1, 'Milo'),
 (2, 'Butter'),
 (3, 'Toilet Paper');
 
-CREATE TABLE `PurchaseHistory` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `userID` int(11) DEFAULT NULL,
-  `groceryID` int(11) DEFAULT NULL,
-  `purchaseDate` date NOT NULL
+CREATE TABLE PurchaseHistory (
+  id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userID int(11) DEFAULT NULL,
+  groceryID int(11) DEFAULT NULL,
+  purchaseDate date NOT NULL
 );
 
-CREATE TABLE `Sharing` (
-  `userID` int(11) NOT NULL,
-  `groceryID` int(11) NOT NULL,
+CREATE TABLE Sharing (
+  userID int(11) NOT NULL,
+  groceryID int(11) NOT NULL,
 
    PRIMARY KEY(userID, groceryID)
 );
 
-INSERT INTO `Sharing` (`userID`, `groceryID`) VALUES
+INSERT INTO Sharing (userID, groceryID) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
@@ -42,12 +42,12 @@ INSERT INTO `Sharing` (`userID`, `groceryID`) VALUES
 (4, 3),
 (5, 3);
 
-CREATE TABLE `Users` (
-  `userID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL
+CREATE TABLE Users (
+  userID int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name varchar(10) DEFAULT NULL
 );
 
-INSERT INTO `Users` (`userID`, `name`) VALUES
+INSERT INTO Users (userID, name) VALUES
 (1, 'Leo'),
 (2, 'Lori'),
 (3, 'Lex'),
