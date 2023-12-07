2b6ba8f4498657936df7a1710eb965c26d1ee461
ILeoI <leoj.whiteside@gmail.com>
2023-12-07 18:07:11 +0930

    added clear lcd file

--- clear_lcd.py
+++ clear_lcd.py
@@ -8,11 +8,15 @@ else:
 
 if __name__ == '__main__':
     display = drivers.Lcd()
-    try:
-        display.lcd_display_string("Tom Stinks", 1)
-        display.lcd_display_string("Tom Stinks", 2)
-        display.lcd_display_string("Tom Stinks", 3)
-        display.lcd_display_string("Tom Stinks", 4)
-    except KeyboardInterrupt:
-        pass
-    display.lcd_clear()
+
+    while 1:
+        try:
+            display.lcd_display_string("Tom Stinks", 1)
+            display.lcd_display_string("Tom Stinks", 2)
+            display.lcd_display_string("Tom Stinks", 3)
+            display.lcd_display_string("Tom Stinks", 4)
+        except KeyboardInterrupt:
+            display.lcd_clear()
+
+    pass
+
